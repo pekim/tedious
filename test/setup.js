64acb61f8be035b33ea481c6791af4f41b1b389b
Arthur Schreiber <schreiber.arthur@googlemail.com>
2024-08-29 23:07:32 +0000

    See if we can figure out what keeps mocha from exiting.

--- test/setup.js
+++ test/setup.js
@@ -2,3 +2,15 @@ require('@babel/register')({
   extensions: ['.js', '.ts'],
   plugins: [ 'istanbul' ]
 });
+
+var wtf = require('wtfnode');
+
+exports.mochaHooks = {
+  afterAll(done) {
+    setTimeout(() => {
+      wtf.dump();
+    }, 1000);
+
+    done();
+  }
+};
